b547b6857ea3a50108a890a81c8f884a68848ee3
Fernando <houspiller@gmail.com>
2015-10-22 09:48:08 -0200

    Inserido nova activity para a formulario de dados da empregada

---
+++ app/src/main/java/br/com/ieb/posgraduacao/empregadafacil/InserirEmpregadoActivity.java
@@ -0,0 +1,13 @@
+package br.com.ieb.posgraduacao.empregadafacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InserirEmpregadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inserir_empregado);
+    }
+}
