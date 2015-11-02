ff042992020f14c616e0a108cf71ee657c7eb8d3
braiannunes <braiannunes10@gmail.com>
2015-11-02 14:36:20 -0200

    - Preparação do layout para o cálculo de férias
    Merge branch 'integracao' into pre-integracao
    
    # Conflicts:
    #	app/src/main/java/br/com/ieb/posgraduacao/empregadafacil/MainActivity.java
    #	app/src/main/java/br/com/ieb/posgraduacao/empregadafacil/Utils/calculosSalario.java
    #	app/src/main/java/br/com/iesb/posgraduacao/activity/CalculaSalarioActivity.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/br/com/iesb/posgraduacao/activity/CalcularFeriasActivity.java
+++ app/src/main/java/br/com/iesb/posgraduacao/activity/CalcularFeriasActivity.java
@@ -1,30 +1,60 @@
 package br.com.iesb.posgraduacao.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import br.com.ieb.posgraduacao.activity.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalcularFeriasActivity extends AppCompatActivity {
+
+    private Spinner spinnerDependentes;
+    private Spinner spinnerFerias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcular_ferias);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        carregarSpinners();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void carregarSpinners() {
+        carregarSpinnerDependentes();
+        carregarSpinnerFerias();
+    }
+
+    private void carregarSpinnerFerias() {
+        spinnerFerias = (Spinner) findViewById(R.id.spinner_dias_ferias);
+        ArrayList<String> listDiasFerias = new ArrayList<String>();
+        for (int i = 10; i <= 30; i++) {
+            listDiasFerias.add(String.valueOf(i));
+        }
+
+        ArrayAdapter<String> adapterDependente = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listDiasFerias);
+        this.spinnerFerias.setAdapter(adapterDependente);
+    }
+
+    private void carregarSpinnerDependentes() {
+        spinnerDependentes = (Spinner) findViewById(R.id.spinner_dependentes);
+        List<String> listDependente = new ArrayList<String>() {{
+            add("00");
+            add("01");
+            add("02");
+            add("03");
+            add("04");
+            add("05");
+            add("06");
+            add("07");
+            add("08");
+            add("09");
+            add("10");
+        }};
+
+        ArrayAdapter<String> adapterDependente = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listDependente);
+        this.spinnerDependentes.setAdapter(adapterDependente);
     }
 
 }
