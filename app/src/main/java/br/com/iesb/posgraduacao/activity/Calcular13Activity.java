23299db641f59784e2cb612687fcd04fa74c9005
Fernando <houspiller@gmail.com>
2015-11-07 14:48:50 -0200

    * Adicionado o pop do 13º para a geração de recibos. Obs.: ainda não foi gerado o recibo.

--- app/src/main/java/br/com/iesb/posgraduacao/activity/Calcular13Activity.java
+++ app/src/main/java/br/com/iesb/posgraduacao/activity/Calcular13Activity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.ieb.posgraduacao.activity.R;
 import br.com.iesb.posgraduacao.util.CalculoSalario;
@@ -22,15 +23,51 @@ public class Calcular13Activity extends AppCompatActivity {
     public void calcular13Salario(View view){
 
         final AlertDialog.Builder msgDialog = new AlertDialog.Builder(this);
+        //segundo dialog com as opções de 1ª e 2ª parcela
+        final AlertDialog.Builder msgDialog2 = new AlertDialog.Builder(this);
+
         msgDialog.setTitle("Resultados");
         msgDialog.setMessage(formaTextoComResultados());
-        msgDialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+
+        //abre as informações com os resultados
+        msgDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         });
-        msgDialog.show();
+
+        //abre outra caixa de diálogo para a geracao dos recibos.
+        msgDialog.setNegativeButton("Gerar Recibo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+                msgDialog2.setTitle("Recibos");
+                msgDialog2.setItems(new CharSequence[]
+                                {"Gerar 1ª Parcela", "Gerar 2ª Parcela"},
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                switch (which) {
+                                    case 0:
+                                        gerarPDF1Parcela();
+                                        Toast.makeText(getBaseContext(), "Gerou 1ª Parcela", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 1:
+                                        gerarPDF2Parcela();
+                                        Toast.makeText(getBaseContext(), "Gerou 2ª Parcela", Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+
+                            }
+                        });
+                msgDialog2.create().show();
+            }
+        });
+
+                msgDialog.show();
     }
 
     //método que retorna o texto completo do relatório gerado
@@ -53,5 +90,13 @@ public class Calcular13Activity extends AppCompatActivity {
         Texto.append(" R$" + calculosSalarioAux.calcularINSS(CalculoSalario.EMPREGADO, Float.parseFloat(etSalarioBruto.getText().toString())) + "\n");
 
         return Texto.toString();
+    }
+
+    public void gerarPDF1Parcela(){
+        //TODO a fazer
+    }
+
+    public void gerarPDF2Parcela(){
+        //TODO a fazer
     }
 }
